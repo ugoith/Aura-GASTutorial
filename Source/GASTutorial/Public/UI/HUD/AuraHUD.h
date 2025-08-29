0116d11db1133ef457ab8cd7619888e6a7489973
0_oUgoith <86e40e031f6e418bb0e057febf9a673b@git.tencent.com>
2025-08-29 18:37:33 +0800

    Widget

---
+++ Source/GASTutorial/Public/UI/HUD/AuraHUD.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "AuraHUD.generated.h"
+
+class UAuraUserWidget;
+/**
+ * 
+ */
+UCLASS()
+class GASTUTORIAL_API AAuraHUD : public AHUD
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY()
+	TObjectPtr<UAuraUserWidget> OverlayWidget;
+protected:
+	virtual void BeginPlay() override;
+private:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UAuraUserWidget> OverlayWidgetClass;
+	
+};
