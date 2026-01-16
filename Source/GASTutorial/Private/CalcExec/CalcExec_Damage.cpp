98e8d2c09b10b31b17675874db7977f342db6ac1
0_oUgoith <86e40e031f6e418bb0e057febf9a673b@git.tencent.com>
2026-01-16 17:53:30 +0800

    Capturing Attributes

--- Source/GASTutorial/Private/CalcExec/CalcExec_Damage.cpp
+++ Source/GASTutorial/Private/CalcExec/CalcExec_Damage.cpp
@@ -4,10 +4,27 @@
 #include "CalcExec/CalcExec_Damage.h"
 
 #include "AbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+struct AuraDamageStatics
+{
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	AuraDamageStatics()
+	{	//false为不快照，需要结合当下的Armor值进行计算，因为在火球命中敌人前可能会被附加一些Armor减弱的Debuff，根据这里被减弱的Armor进行计算受伤后的逻辑
+		//这里的宏用于定义捕获规则  从哪个属性集里拿，拿哪个属性，从哪个目标身上拿，是否是实时的数据 false 实时，true非实时
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet,Armor,Target,false); 
+	}
+};
+
+static const AuraDamageStatics& DamageStatics()
+{
+	static AuraDamageStatics DamageStatics;
+	return DamageStatics; 
+}
 
 UCalcExec_Damage::UCalcExec_Damage()
 {
-	
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
 }
 
 void UCalcExec_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
@@ -20,5 +37,15 @@ void UCalcExec_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	const AActor* TargetAvatar = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
 
 	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
-	
+	const FGameplayTagContainer* SourceTags = Spec.CapturedTargetTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+	FAggregatorEvaluateParameters EvaluateParameters;
+	EvaluateParameters.SourceTags = SourceTags;
+	EvaluateParameters.TargetTags = TargetTags;
+	float Armor = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef,EvaluateParameters,Armor);
+	++Armor;
+
+	FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty,EGameplayModOp::Additive,Armor);
+	OutExecutionOutput.AddOutputModifier(EvaluatedData);
 }
