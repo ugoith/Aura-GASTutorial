85fcfab08e21358f414adc05648078c0d94a02b3
Ugoith <2316489400@qq.com>
2026-01-29 00:15:25 +0800

    Implementing Armor And ArmorPenetration

--- Source/GASTutorial/Private/CalcExec/CalcExec_Damage.cpp
+++ Source/GASTutorial/Private/CalcExec/CalcExec_Damage.cpp
@@ -10,12 +10,14 @@
 struct AuraDamageStatics
 {
 	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	DECLARE_ATTRIBUTE_CAPTUREDEF(ArmorPenetration);
 	DECLARE_ATTRIBUTE_CAPTUREDEF(BlockChance);
 	AuraDamageStatics()
 	{	//false为不快照，需要结合当下的Armor值进行计算，因为在火球命中敌人前可能会被附加一些Armor减弱的Debuff，根据这里被减弱的Armor进行计算受伤后的逻辑
 		//这里的宏用于定义捕获规则  从哪个属性集里拿，拿哪个属性，从哪个目标身上拿，是否是实时的数据 false 实时，true非实时
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet,Armor,Target,false); 
 		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet,BlockChance,Target,false);
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet,ArmorPenetration,Source,false);
 	}
 };
 
@@ -47,12 +49,25 @@ void UCalcExec_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 	EvaluateParameters.SourceTags = SourceTags;
 	EvaluateParameters.TargetTags = TargetTags;
 	
+	float TargetArmor = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef, EvaluateParameters, TargetArmor);
+	TargetArmor = FMath::Max<float>(TargetArmor,0.f);
+
 	float TargetBlockChance = 0.f;
-	float Damage = Spec.GetSetByCallerMagnitude(FAuraGameplayTags::Get().Damage);
 	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().BlockChanceDef,EvaluateParameters,TargetBlockChance);
 	TargetBlockChance = FMath::Max<float>(TargetBlockChance,0.f);
-	const bool bBlock = FMath::RandRange(1.f,100.f) < TargetBlockChance;
-	Damage = bBlock ? Damage / 2.f : Damage;
+
+	float SourceArmorPenetration = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorPenetrationDef,EvaluateParameters,SourceArmorPenetration);
+	SourceArmorPenetration = FMath::Max<float>(SourceArmorPenetration,0.f);
+	
+	float Damage = Spec.GetSetByCallerMagnitude(FAuraGameplayTags::Get().Damage);
+
+	const float EffectiveArmor = TargetArmor *= (100 - SourceArmorPenetration * 0.25f) /100.f;
+	Damage *= (100 - EffectiveArmor / 3.f) /100.f;
+
+	/*const bool bBlock = FMath::RandRange(1.f,100.f) < TargetBlockChance;
+	Damage = bBlock ? Damage / 2.f : Damage;*/
 	
 	FGameplayModifierEvaluatedData EvaluatedData(UAuraAttributeSet::GetIncomingDamageAttribute(),EGameplayModOp::Additive,Damage);
 	OutExecutionOutput.AddOutputModifier(EvaluatedData);
